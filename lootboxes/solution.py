bca8e0a61d2c3b89aa09711633ad24f7c45f083d
Uladzislau Khamkou <kham@tuta.io>
2022-03-06 14:50:22 +0300

    prettify code for lootboxes

--- lootboxes/solution.py
+++ lootboxes/solution.py
@@ -2,6 +2,7 @@ from functools import cache
 from math import inf
 from fractions import Fraction
 
+
 @cache
 def dp(prices, n, x):
     if len(prices) == 0:
@@ -13,7 +14,7 @@ def dp(prices, n, x):
     # tactic: BUY MINIMAL
     for i in range(len(prices)):
         if prices[i] == min_price:
-            prices1 = prices[:i] + prices[i + 1 : ]
+            prices1 = prices[:i] + prices[i + 1 :]
             break
     prices1 = (min_price + dp(prices1, n, x)[0], "buy")
     best_tactic = min(best_tactic, prices1)
@@ -22,15 +23,17 @@ def dp(prices, n, x):
     avg_price = 0
     casino_price = (1 + Fraction(n) / len(prices)) * x / 2
     for i in range(len(prices)):
-        prices2 = prices[:i] + prices[i + 1 : ]
+        prices2 = prices[:i] + prices[i + 1 :]
         avg_price += (dp(prices2, n, x)[0] + casino_price) / len(prices)
     best_tactic = min(best_tactic, (avg_price, "casino"))
     return best_tactic
 
-def get_tactic(prices, x=1, n = None):
+
+def get_tactic(prices, x=1, n=None):
     prices = tuple(sorted(Fraction(p) for p in prices))
     n = n if n is not None else len(prices)
     return dp(prices, n, Fraction(x))
+
 
 prices = list(map(Fraction, input("Input prices of things: ").split()))
 casino_price = Fraction(input("Input price of lootbox: "))
