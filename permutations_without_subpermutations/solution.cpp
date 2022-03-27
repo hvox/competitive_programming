e5bb36ecac3f87ad8dbd859f69cb70cd2beb1cdb
Uladzislau Khamkou <kham@tuta.io>
2022-03-27 21:51:50 +0300

    precompute subperms-free permutations

--- permutations_without_subpermutations/solution.cpp
+++ permutations_without_subpermutations/solution.cpp
@@ -1,84 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int64_t max_int = 127;
-
-map<int64_t, int64_t> factorial_cache;
-int64_t factorial(int64_t n) {
-	if (n < 2) return 1;
-	if (factorial_cache.find(n) != factorial_cache.end()) {
-		return factorial_cache[n];
+vector<int64_t> precompute_everything(const int64_t max_n, int64_t max_int) {
+	vector<int64_t> factorials(max_n + 1);
+	factorials[0] = 1;
+	for (int n = 1; n <= max_n; n++) {
+		factorials[n] = (factorials[n - 1] * n) % max_int;
 	}
-	int64_t result = (factorial(n - 1) * n) % max_int;
-	factorial_cache[n] = result;
-	return result;
-}
-
-map<int64_t, int64_t> nonprefixed_cache;
-int64_t nonprefixed(int64_t n) {
-	if (nonprefixed_cache.find(n) != nonprefixed_cache.end()) {
-		return nonprefixed_cache[n];
-	}
-	int64_t result = 0;
-	for (int k = 1; k < n; k++) {
-		result += factorial(n - k) * nonprefixed(k);
-		result %= max_int;
-	}
-	result = (factorial(n) - result) % max_int;
-	nonprefixed_cache[n] = result;
-	return result;
-}
-
-map<tuple<int64_t, int64_t>, int64_t> intervals_cache;
-int64_t intervals(int64_t k, int64_t n) {
-	if (k > n) return 0;
-	if (k == n) return 1;
-	if (intervals_cache.find({k, n}) != intervals_cache.end()) {
-		return intervals_cache[{k, n}];
-	}
-	int64_t result = 0;
-	for (int size = 1; size <= n; size++) {
-		result += intervals(k - 1, n - size) * factorial(size);
-		result %= max_int;
-	}
-	intervals_cache[{k, n}] = result;
-	return result;
-}
-
-map<int64_t, int64_t> nonsp_perms_cache;
-int64_t nonsp_perms(int64_t n) {
-	if (nonsp_perms_cache.find(n) != nonsp_perms_cache.end()) {
-		return nonsp_perms_cache[n];
-	}
-	int64_t result = 0;
-	if (n > 2) {
+	vector<int64_t> noprefixes(max_n + 1);
+	for (int n = 0; n <= max_n; n++) {
+		int64_t sum = 0;
 		for (int k = 1; k < n; k++) {
-			result += factorial(n - k) * nonprefixed(k);
-			result %= max_int;
+			sum += (factorials[n - k] * noprefixes[k]) % max_int;
+			sum %= max_int;
 		}
-		result *= 2;
-		result %= max_int;
+		noprefixes[n] = (factorials[n] - sum + max_int) % max_int;
 	}
-	for (int k = 3; k < n; k++) {
-		result += intervals(k, n) * nonsp_perms(k);
-		result %= max_int;
+	vector<int64_t> intervals((max_n + 1) * (max_n + 1));
+	int64_t off = max_n + 1;
+	for (int n = 0; n <= max_n; n++) {
+		intervals[n * off + 0] = 0;
+		for (int k = 1; k < n; k++) {
+			int64_t sum = 0;
+			for (int size = 1; size <= n; size++) {
+				int64_t term = intervals[(n - size) * off + k - 1];
+				term = (term * factorials[size]) % max_int;
+				sum = (sum + term) % max_int;
+			}
+			intervals[n * off + k] = sum;
+		}
+		intervals[n * off + n] = 1;
+		for (int k = n + 1; k <= max_n; k++) {
+			intervals[n * off + k] = 0;
+		}
 	}
-	result = (factorial(n) - result) % max_int;
-	nonsp_perms_cache[n] = result;
-	return result;
+	vector<int64_t> spfree_perms(max_n + 1);
+	for (int n = 0; n <= max_n; n++) {
+		int64_t sum = 0;
+		if (n > 2) {
+			for (int k = 1; k < n; k++) {
+				sum += (factorials[n - k] * noprefixes[k]) % max_int;
+				sum %= max_int;
+			}
+			sum = (sum * 2) % max_int;
+		}
+		for (int k = 3; k < n; k++) {
+			sum += (spfree_perms[k] * intervals[n * off + k]) % max_int;
+			sum %= max_int;
+		}
+		spfree_perms[n] = (factorials[n] - sum + max_int) % max_int;
+	}
+	return spfree_perms;
 }
+
 
 int main() {
 	cout << "Number of sizes: ";
 	int t;
 	cin >> t;
 	cout << "Max int: ";
+	int64_t max_int;
 	cin >> max_int;
+	vector<int64_t> spfree_perms = precompute_everything(400, max_int);
 	while (t--) {
 		cout << "Input size of permutation: ";
 		int64_t n;
 		cin >> n;
 		cout << "Number of permutation without subpermutations: ";
-		cout << nonsp_perms(n) << endl;
+		cout << spfree_perms[n] << endl;
 	}
 }
