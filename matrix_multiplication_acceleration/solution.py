160a9dc4a778c9bbecb4e510fe46618c70094b82
Uladzislau Khamkou <kham@tuta.io>
2022-10-23 00:14:30 +0300

    hch2210: make all the ones into zeros

--- matrix_multiplication_acceleration/solution.py
+++ matrix_multiplication_acceleration/solution.py
@@ -1,10 +1,6 @@
-# score: 6447912
-import random
-import numpy
+# score: 16780600
+import itertools
 import sys
-from random import randint
-
-random.seed(0)
 
 
 def get_score(original_matrices, matrices):
@@ -12,7 +8,7 @@ def get_score(original_matrices, matrices):
     ys, products = matmult(matrices)
     acc = sum(sum(y / x for x, y in zip(xs, ys)) for xs, ys in zip(xs, ys)) / n / n
     param = products / orig_products
-    return int(max(0, 1e7 * ((acc - 1) / 2 + 1 - param) / 2))
+    return int(max(0, 1e7 * ((acc - 1) / 2 + 1 - param) / 2)) * (acc >= 0.6)
 
 
 def matmult(matrices):
@@ -25,18 +21,19 @@ def matmult(matrices):
                     if x[i][k] and y[k][j]:
                         new_x[i][j] += x[i][k] * y[k][j]
                         operations += 1
-        x = numpy.array(new_x)
+        x = new_x
     return x, operations
 
 
 d, n = map(int, input().split())
-matrices = [numpy.array([list(map(int, input().split())) for _ in range(n)]) for _ in range(d)]
-original_matrices = [numpy.array(matrix) for matrix in matrices]
-for _ in range(int(n**2)):
-    x, y, z = (randint(0, d - 1), randint(0, n - 1), randint(0, n - 1))
-    matrices[x][y][z] *= (matrices[x][y][z] > 1)
-for matrix in matrices:
-    for row in matrix:
-        print(" ".join(map(str, row)))
+matrices = [[list(map(int, input().split())) for _ in range(n)] for _ in range(d)]
+original_matrices = [[row.copy() for row in matrix] for matrix in matrices]
+for x in range(d - (n < 5)):
+    for y, z in itertools.product(range(n), repeat=2):
+        matrices[x][y][z] *= matrices[x][y][z] > 1
 if sys.stdout.isatty():
     print(get_score(original_matrices, matrices))
+else:
+    for matrix in matrices:
+        for row in matrix:
+            print(" ".join(map(str, row)))
